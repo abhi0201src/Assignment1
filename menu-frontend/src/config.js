0cd715f56c09d72b9e31ff7783f5c7070e96309d
Abhishek S <abhishek.sa.2001@gmail.com>
2025-08-12 11:07:48 +0530

    Update config.js

--- menu-frontend/src/config.js
+++ menu-frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: 'http://django-nextjs-alb-2114475505.us-west-2.elb.amazonaws.com:8000'
+  apiUrl: 'django-nextjs-alb-734957308.us-west-2.elb.amazonaws.com:8000'
 };
 
 export default config;
